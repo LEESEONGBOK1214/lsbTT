ac37ed1b19511828f4c42e6e38c9880409c8b70d
nyximos <nyximos@gmail.com>
2020-12-28 21:56:36 +0900

    update 소미/Ex01

--- test01/src/소미/Ex01.java
+++ test01/src/소미/Ex01.java
@@ -3,8 +3,9 @@ package ¼Ò¹Ì;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i=0; i<5; i++) {
+			System.out.println("somi");
+		}
 	}
 
 }
