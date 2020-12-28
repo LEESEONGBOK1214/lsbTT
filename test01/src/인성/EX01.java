07c0a1c6f3b28adbead13ba0bf5392c5a4faa577
home <home@220.89.94.153>
2020-12-28 21:50:46 +0900

    create EX01

---
+++ test01/src/인성/EX01.java
@@ -0,0 +1,11 @@
+package 인성;
+
+public class EX01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++)
+			System.out.println("인성");
+	}
+
+}
