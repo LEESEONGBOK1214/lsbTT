de814a590b87ab3288f4e09e44a32c40fe72b38b
home <home@220.89.94.153>
2020-12-28 21:53:03 +0900

    update 인성/EX01

--- test01/src/인성/EX01.java
+++ test01/src/인성/EX01.java
@@ -5,7 +5,7 @@ public class EX01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 5; i++)
-			System.out.println("ÀÎ¼º");
+			System.out.println("Inseong");
 	}
 
 }
